c6f1f400472a0d24f64a56e36efc99c696071570
Jeet <Jeet@Metapercept.com>
2021-01-22 10:52:25 +0530

    template.json

---
+++ template/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '992fb25bf8e277ac83c8a565ae3fa372'),
+    },
+  },
+});
